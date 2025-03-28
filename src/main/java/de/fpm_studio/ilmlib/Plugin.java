aa0212b08b96eab348ebc739066970b516930fda
ItsLeMax <lemaxyt1243@gmail.com>
2025-03-28 01:08:13 +0100

    Template enum as class, docs pom and packages update, better suppression, QoL

---
+++ src/main/java/de/fpm_studio/ilmlib/Plugin.java
@@ -0,0 +1,17 @@
+package de.fpm_studio.ilmlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Contains the {@code onEnable} method for plugins that want the library to be read from the plugins folder
+ * <br>
+ * Enthält die {@code onEnable}-Methode für Plugins, wo die Bibliothek aus dem Pluginsordner gelesen werden soll
+ *
+ * @author ItsLeMax
+ */
+public final class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Placeholder
+    }
+}
---
+++ src/main/java/de/fpm_studio/ilmlib/util/Template.java
@@ -0,0 +1,13 @@
+package de.fpm_studio.ilmlib.util;
+
+/**
+ * Contains the template enums
+ *
+ * @author ItsLeMax
+ */
+public enum Template {
+    SUCCESS,
+    WARNING,
+    ERROR,
+    INFO
+}
